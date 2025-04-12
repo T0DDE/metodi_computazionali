2fa3e83ee167884e193be46042d90e2a5248aa68
Gabri <gabriele.todde@gmail.com>
2025-04-12 19:36:35 +0200

    Appunti primo approccio all'esercizio 4

---
+++ hands-on/exercise4_classes/solution/my_module/my_class.py
@@ -0,0 +1,28 @@
+class rational () :
+    """
+    Questa classe serve ad approssimare un numero in input con un numero razionale. 
+    """
+    def __init__ ( self, num, precision = 1.e-5 ) :
+        pass
+    def __str__ ( self ) :
+    	pass
+    def __repr__ ( self ) :
+    	pass
+    def __add__ ( self ) :
+    	pass
+    # [ ... ] overload all the arithmetic operators
+    def __eq__ ( self ) :
+    	pass
+    # [ ... ] overload all the comparison operators
+
+    ####################################################
+    # Optional functions:
+    
+    def __hash__ ( self ) :
+    	pass
+    def to_integer_low ( self ) :
+    	"""Documentation of function `to_integer_low`"""
+    	pass
+    def to_integer_upp ( self ) :
+    	"""Documentation of function `to_integer_upp`"""
+    	pass
